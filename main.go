01a77ab5c15afb7cb111a50e71ee793face4c657
NickDiPreta <nicholasdipreta@gmail.com>
2023-07-03 14:00:03 -0400

    part one fin

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 
+	// Anything other than 'goodbye'
 	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
 		log.Println("Hello World")
+	})
+
+	// Function called when goodbye path is called
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
 	})
 
 	http.ListenAndServe(":9090", nil)
